c1cb3186ca31b1e61d014fbfff76cf57e7f13451
gujaKhela <khelashviliguja@gmail.com>
2024-01-26 13:05:20 +0400

    just create react project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import siteLogo from '../public/favicon.ico'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="/" target="_blank">
+          <img src={siteLogo} className="" alt="logo to my site" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
