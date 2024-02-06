a0b8951148bf88a77efe8e32993d05333df3232d
gujaKhela <khelashviliguja@gmail.com>
2024-02-07 00:08:13 +0400

    Complite the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import siteLogo from "../assets/siteLogo.svg";
 import { useLocation, useNavigate } from "react-router-dom";
+import {getAuth, onAuthStateChanged}from "firebase/auth"
 
 export const Header = () => {
+
+  const [checkSignIn,setCheckSignIn] = useState("Sign In")
+  const auth = getAuth();
 
   const location = useLocation();
   const navigate = useNavigate();
@@ -12,6 +16,17 @@ export const Header = () => {
       return true;
     }
   }
+
+
+  useEffect(()=>{
+    onAuthStateChanged(auth,(user)=>{
+      if(user) {
+        setCheckSignIn("Profile")
+      } else{
+        setCheckSignIn("Sign In")
+      }
+    })
+  },[auth])
 
   return (
     <div className="bg-white border-b shadow-sm sticky top-0 z-50">
@@ -45,10 +60,10 @@ export const Header = () => {
             </li>
             <li
               className={"cursor-pointer py-3 text-sm font-semibold  " +
-             (matchPathName("/sign-in") ? "text-black border-b-[3px] border-b-red-500" : "text-gray-400 ")}
-              onClick={() => navigate("/sign-in")}
+             ((matchPathName("/sign-in")||matchPathName("/profile") )? "text-black border-b-[3px] border-b-red-500" : "text-gray-400 ")}
+              onClick={() => navigate("/profile")}
             >
-              Sign In
+              {checkSignIn}
             </li>
           </ul>
         </div>
